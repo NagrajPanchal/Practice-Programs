3c107da6bfae7b6eff0bca388bf6af9b763334e8
Nagraj <nagrajsubhashpanchal.gmail.com>
2022-08-18 10:20:47 +0530

    Update: Number Prime

--- src/Number_Prime.java
+++ src/Number_Prime.java
@@ -8,20 +8,25 @@ public class Number_Prime
     public static void main (String[]args)
     {
         int i,n,count=0;
-        boolean isprime = true;
         System.out.println("Enter any number : ");
         Scanner in = new Scanner(System.in);
         n = in.nextInt();
         // 0 and 1 are not prime numbers also, negative numbers are not prime
-          for (i = 2; i <= n; i++) {
-              if (n % i == 0) {
+          for (i = 1; i <= n; i++)
+          {
+              if (n % i == 0)
+              {
                   count++;
               }
           }
               if (count == 2)
-                  System.out.println("Number is prime");
+              {
+                  System.out.println(+n+" Number is 'prime'");
+              }
               else
-                  System.out.println("Not Prime");
+              {
+                  System.out.println(+n+" is not 'Prime'");
+              }
 
         }
 
