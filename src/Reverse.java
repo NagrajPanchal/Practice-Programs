d698be4c5407a66e21d20fe7a8e7e279f34606a5
Nagraj <nagrajsubhashpanchal.gmail.com>
2022-09-02 09:31:05 +0530

    Refactor : Reverse Number

--- src/Reverse.java
+++ src/Reverse.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class Reverse {
+// Reverse Number
+
+import java.util.Scanner;
+
+public class Reverse
+{
+    public static void main (String[]args)
+    {
+
+        //variables initialization
+        int reverse = 0, rem;
+        System.out.print("Enter any number : ");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        //loop to find reverse number
+        while (num != 0)
+        {
+            rem = num % 10;
+            reverse = reverse *  10 + rem;
+            num /= 10; // num = num / 10
+        };
+        System.out.println ("Reversed Number: " + reverse);
+    }
 }
