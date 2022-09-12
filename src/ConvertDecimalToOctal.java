f2f855547f04730a8d3a233fb6321a3687d0032f
Nagraj <nagrajsubhashpanchal.gmail.com>
2022-09-12 17:04:29 +0530

    Modify : COnvert Decimal NUmber to Octal Numbers

--- src/ConvertDecimalToOctal.java
+++ src/ConvertDecimalToOctal.java
@@ -8,7 +8,16 @@ public class ConvertDecimalToOctal
         Scanner scanner =new Scanner(System.in);
         System.out.print("Enter Decimal Number : ");
         int decimal = scanner.nextInt();
-        int octal = 0;
-
+        int octal[] = new int[20];
+        int i = 0;
+        while (decimal > 0)
+        {
+            int rem = decimal % 8;
+            octal[i++] = rem;
+            decimal = decimal/8;
+        }
+        System.out.print("Octal Number : ");
+        for (int j = i-1; j>= 0; j--)
+            System.out.print(octal[j] + "");
     }
 }
